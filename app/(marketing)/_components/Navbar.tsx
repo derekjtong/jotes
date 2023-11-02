3db1e661e055bc5bc11b1d74916e9c6b402c1a49
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-02 18:30:13 -0400

    Added log in and sign up buttons

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,15 +4,32 @@ import { useScrollTop } from "@/hooks/useScrollTop";
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
 import { ModeToggle } from "../../../components/ModeToggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, SignUpButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 function Navbar() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
-    <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6 dark:bg-[#1F1F1F]", scrolled && "border-b shadow-sm")}>
+    <div className={cn("fixed top-0 z-50 flex w-full items-center bg-background p-6 dark:bg-[#1F1F1F]", scrolled && "border-b shadow-sm")}>
       <Logo />
-      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        Login <ModeToggle />
+      <div className="flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
+        {isLoading && <p>Loading...</p>}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button size="sm">Get Jotes Free</Button>
+            </SignUpButton>
+          </>
+        )}
+        <ModeToggle />
       </div>
     </div>
   );
